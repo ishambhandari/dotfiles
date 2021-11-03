ca187cc763e36b5b07265e4c48fae2eb32763b3d
isham bhandari <ishambhandari007@gmail.com>
2021-11-03 17:01:34 +0545

    emacs config changes

--- emacs/init.el
+++ emacs/init.el
@@ -11,9 +11,30 @@
 (setq visible-bell nil)
 (setq mac-command-key-is-meta t)
 (setq display-line-numbers 'relative)
+(setq gc-cons-threshold 100000000)
+(set-default 'truncate-lines t)
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 115)
 
-(set-face-attribute 'default nil :font "Fira Code Retina" :height 150)
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )
+;;general
+(delete-selection-mode t)
+(setq gc-cons-threshold 100000000)
+(setq org-log-done t)
+(setq read-process-output-max (* 1024 1024))
+(setq auto-save-default nil)
+(setq make-backup-file nil)
+(setq create-lockfiles nil)
 
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
 ;;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (use-package dracula-theme)
 (load-theme 'dracula t)
@@ -23,6 +44,7 @@
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "C-c m") 'mu4e)
 (global-linum-mode 1)
   
   
@@ -122,9 +144,9 @@
   (evil-collection-init))
 
 (require 'key-chord)
+(setq key-chord-two-keys-delay 0.5)
+(key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
 (key-chord-mode 1)
-(key-chord-define evil-insert-state-map  "jk" 'evil-normal-state)
-
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
 	 ("C-x b" . counsel-ibuffer)
@@ -151,6 +173,9 @@
   :custom (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
 (use-package forge) 
+;; auto save files
+(setq auto-save-file-name-transforms
+  `((".*" "~/.emacs-saves/" t)))
 
 
 
@@ -171,22 +196,13 @@
   :mode (("\\.js\\'" . web-mode)
 	 ("\\.jsx\\'" . web-mode)
 	 ("\\.ts\\'" . web-mode)
-	 ("\\.jsx\\'" . web-mode)
+	 ("\\.tsx\\'" . web-mode)
 	 ("\\.html\\'" . web-mode))
   :commands web-mode)
-(add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode)) 
-(setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+;; (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode)) 
+;; (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
 
-;;flycheck
-
-(use-package flycheck)
-(setq-default flycheck-disabled-checkers
-              (append flycheck-disabled-checkers
-                      '(javascript-jshint json-jsonlist)))
-;; Enable eslint checker for web-mode
-(flycheck-add-mode 'javascript-eslint 'web-mode)
-;; Enable flycheck globally
-(add-hook 'after-init-hook #'global-flycheck-mode)
+;;rjsx mode
 
 
 ;;company
@@ -196,24 +212,30 @@
   :ensure t
   :config (global-company-mode t))
 
-;;lsp mode
-
 (setq lsp-log-io nil)
 (setq lsp-keymap-prefix "C-c l")
 (setq lsp-restart 'auto-restart)
 (setq lsp-ui-sideline-show-diagnostics t)
-(setq lsp-io-sideline-show-hover t)
-(setq lst-io-sideline-show-code-actions t)
+(setq lsp-ui-sideline-show-hover t)
+(setq lsp-ui-sideline-show-code-actions t)
+
+
+
+;;lsp mode
 (use-package lsp-mode
-  :commands (lsp-deffered)
+  :ensure t
   :hook (
 	 (web-mode . lsp-deferred)
 	 (lsp-mode . lsp-enable-which-key-integration)
-	 ))
+	 )
+  :commands lsp-deferred)
+
 (use-package lsp-ui
   :ensure t
-  :commands lsp-ui-mode
-  )
+  :commands lsp-ui-mode)
+
+
+
 
 (defun enable-minor-mode (my-pair)
   "Enable minor mode if filename match the regexp.  MY-PAIR is a cons cell (regexp . minor-mode)."
@@ -232,27 +254,54 @@
 
 (add-hook 'js2-mode-hook 'prettier-js-mode)
 (add-hook 'web-mode-hook 'prettier-js-mode)
+;;email client
+
+(use-package mu4e
+  :ensure nil
+  :load-path "/usr/share/emacs/site-lisp/mu4e/"
+  ;; :defer 20 ; Wait until 20 seconds after startup
+  :config
+
+  ;; This is set to 't' to avoid mail syncing issues when using mbsync
+  (setq mu4e-change-filenames-when-moving t)
+
+  ;; Refresh mail using isync every 10 minutes
+  (setq mu4e-update-interval (* 10 60))
+  (setq mu4e-get-mail-command "mbsync -a")
+  (setq mu4e-root-maildir "~/Mail")
+
+  (setq mu4e-drafts-folder "/[Gmail]/Drafts")
+  (setq mu4e-sent-folder   "/[Gmail]/Sent Mail")
+  (setq mu4e-refile-folder "/[Gmail]/All Mail")
+  (setq mu4e-trash-folder  "/[Gmail]/Trash")
+
+  (setq mu4e-maildir-shortcuts
+      '(("/Inbox"             . ?i)
+        ("/[Gmail]/Sent Mail" . ?s)
+        ("/[Gmail]/Trash"     . ?t)
+        ("/[Gmail]/Drafts"    . ?d)
+        ("/[Gmail]/All Mail"  . ?a))))
 
 ;; (eval-after-load 'js-mode
 ;;   '(add-hook 'js-mode-hook #'add-node-modules-path))
 
   (evil-ex-define-cmd "q" 'kill-this-buffer)
-;; (use-package rjsx-mode
-;;   :ensure t
-;;   :mode "\\.js\\'")
 
-;; (defun setup-tide-mode()
-;;   (interactive)
-;;   (tide-setup)
-;;   (flycheck-mode +1)
-;;   (setq flycheck-check-syntax-automatically '(save mode-enabled))
-;;   (tide-hl-identifier-mode +1)
-;;   (company-mode +1))
+(use-package rjsx-mode
+  :ensure t
+  :mode "\\.js\\'")
+(defun setup-tide-mode()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
 
-;; (use-package tide
-;;   :ensure t
-;;   :after (rjsx-mode company flycheck)
-;;   :hook (rjsx-mode . setup-tide-mode))
+(use-package tide
+  :ensure t
+  :after (rjsx-mode company flycheck)
+  :hook (rjsx-mode . setup-tide-mode))
 
 ;; (use-package prettier-js
 ;;   :commands (prettier-js-mode prettier)
@@ -289,6 +338,56 @@
 ;; (add-hook 'js2-mode-hook (lambda ()
 ;;   (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
 
+;; orgmode
+
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+;; Org Mode Configuration ------------------------------------------------------
+
+(defun efs/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+  (efs/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+(setq org-agenda-files '("~/notes.org"
+			 "~/tasks.org"
+			 ))
+(global-set-key (kbd "C-c a") 'org-agenda)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -297,7 +396,7 @@
  '(custom-safe-themes
    '("d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" default))
  '(package-selected-packages
-   '(eslint-fix exec-path-from-shell dracula-theme add-node-modules-path prettier evil-commentary highlight-indent-guides typescript-mode lsp-mode forge evil-magit counsel-projectile projectile which-key use-package telephone-line rainbow-delimiters key-chord hydra general evil-collection doom-modeline counsel command-log-mode)))
+   '(org-bullets tide rg ripgrep mu4e-views eslint-fix exec-path-from-shell dracula-theme add-node-modules-path prettier evil-commentary highlight-indent-guides typescript-mode forge evil-magit counsel-projectile projectile which-key use-package telephone-line rainbow-delimiters key-chord hydra general evil-collection doom-modeline counsel command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
